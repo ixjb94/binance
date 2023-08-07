0f8697726620475ad34defca09db24c8891daa99
IX JB <ixjb94@gmail.com>
2023-08-07 02:03:01 +0000

    try catch wrap - let to const

--- src/core/websocket.ts
+++ src/core/websocket.ts
@@ -35,143 +35,196 @@ export class Websocket extends EventEmitter {
     }
 
     // ######################################## Overwrite addListener & on JSDoc
-    // addListener(eventName, callback) {
-    //     super.addListener(eventName, callback)
-    // }
 
-    // on(eventName, callback) {
-    //     super.on(eventName, callback)
-    // }
+    // @ts-ignore
+    addListener(eventName, callback) {
+        try {
+            super.addListener(eventName, callback)
+        } catch (error) {
+            console.log("Error addListener:", error)
+        }
+    }
+
+    // @ts-ignore
+    on(eventName, callback) {
+        try {
+            super.on(eventName, callback)
+        } catch (error) {
+            console.log("Error On:", error)
+        }
+    }
     // ############################################
     async sleep(ms: number) {
         try {
             return new Promise(resolve => setTimeout(resolve, ms))
-        } catch { }
+        } catch (error) {
+            console.log("Error Sleep:", error)
+        }
     }
 
     unsubscribe(wsID: number) {
-        let topic = this.wsTopics.get(wsID)
+        try {
+            const topic = this.wsTopics.get(wsID)
 
-        if (topic) {
+            if (topic) {
 
-            let ws = topic.ws
-            let request = topic.request
+                const ws = topic.ws
+                const request = topic.request
 
-            console.log(`Unsubscribe to ${request.params}`)
+                console.log(`Unsubscribe to ${request.params}`)
 
-            request.method = "UNSUBSCRIBE"
+                request.method = "UNSUBSCRIBE"
 
-            ws.send(JSON.stringify(request))
-            ws.close(1000, `Unsubscribed: ${request.params}`)
-            this.wsTopics.delete(wsID)
+                ws.send(JSON.stringify(request))
+                ws.close(1000, `Unsubscribed: ${request.params}`)
+                this.wsTopics.delete(wsID)
+            }
+        } catch (error) {
+            console.log("Error Unsubscribe:", error)
         }
     }
 
     subscribe(params: object, id: number, eventName = "DATA") {
-        let URL = this.wsBaseURL
-        if (this.isTestNet) {
-            console.log("## Websocket: Test Net ##")
-            URL = this.wsBaseURLTest
-        }
 
-        let ws = new WsClient(URL + "/ws")
-
-        let request = {
-            method: "SUBSCRIBE",
-            params,
-            id,
-        }
-
-        ws.on("ping", () => {
-            ws.ping()
-            ws.pong()
-        })
-
-        ws.on("open", () => {
-
-            console.log(`Subscribed to ${request.params}`)
-
-            // Ask binance for subscription
-            ws.send(JSON.stringify(request))
-
-            // Add to subscribe list
-            this.wsTopics.set(request.id, { ws, request })
-
-            this.emit(eventName, ws, {
-                api_key: this.api_key,
-                api_secret: this.api_secret,
-            })
-        })
-
-        ws.on("close", (code, reason) => {
-            console.log(`Websocket Closed - Code: ${code} - Reason: ${reason}`)
-
-            let wsReference = this.wsTopics.get(request.id)
-            if (wsReference) {
-                
-                let reconnectSleep: number = this.reconnectSleep ?? 3000
-
-                console.log(`Reconnecting after ${reconnectSleep} ms`)
-                this.sleep(reconnectSleep).then(() => {
-                    this.subscribe(params, request.id, eventName)
-                })
+        try {
+            let URL = this.wsBaseURL
+            if (this.isTestNet) {
+                console.log("## Websocket: Test Net ##")
+                URL = this.wsBaseURLTest
             }
-        })
 
-        ws.on("error", (error) => console.log("Error Happens", error))
+            const ws = new WsClient(URL + "/ws")
+
+            const request = {
+                method: "SUBSCRIBE",
+                params,
+                id,
+            }
+
+            ws.on("ping", () => {
+                try {
+                    ws.ping()
+                    ws.pong()
+                } catch { }
+            })
+
+            ws.on("open", () => {
+                try {
+                    console.log(`Subscribed to ${request.params}`)
+
+                    // Ask binance for subscription
+                    ws.send(JSON.stringify(request))
+
+                    // Add to subscribe list
+                    this.wsTopics.set(request.id, { ws, request })
+
+                    this.emit(eventName, ws, {
+                        api_key: this.api_key,
+                        api_secret: this.api_secret,
+                    })
+                } catch { }
+            })
+
+            ws.on("close", (code, reason) => {
+                try {
+                    console.log(`Websocket Closed - Code: ${code} - Reason: ${reason}`)
+
+                    const wsReference = this.wsTopics.get(request.id)
+                    if (wsReference) {
+
+                        const reconnectSleep: number = this.reconnectSleep ?? 3000
+
+                        console.log(`Reconnecting after ${reconnectSleep} ms`)
+                        this.sleep(reconnectSleep).then(() => {
+                            this.subscribe(params, request.id, eventName)
+                        })
+                    }
+                } catch { }
+            })
+
+            ws.on("error", (error) => {
+                try {
+                    console.log("Error Happens", error)
+                } catch { }
+            })
+        } catch (error) {
+            console.log("Error Subscribe:", error)
+        }
     }
 
     disconnect(path: string) {
-        let wsRef = this.wsLists.get(path)
-        if (wsRef.ws) {
-            let ws = wsRef.ws
-            ws.close()
-            this.wsLists.delete(path)
+        try {
+            const wsRef = this.wsLists.get(path)
+
+            if (wsRef.ws) {
+                const ws = wsRef.ws
+                ws.close()
+                this.wsLists.delete(path)
+            }
+        } catch (error) {
+            console.log("Error disconnect:", error)
         }
     }
 
     connect(path: string, eventName = "DATA") {
 
-        let URL = this.wsBaseURL
-        if (this.isTestNet) {
-            console.log("## Websocket: Test Net ##")
-            URL = this.wsBaseURLTest
-        }
-
-        let ws = new WsClient(URL + path)
-
-        ws.on("open", () => {
-            console.log(`Connection Opened for: ${path}`)
-
-            // Add to connection list
-            this.wsLists.set(path, { ws, eventName })
-
-            this.emit(eventName, ws, {
-                api_key: this.api_key,
-                api_secret: this.api_secret,
-            })
-        })
-
-        ws.on("close", (code: number, reason: string) => {
-            console.log(`Connection closed for: ${path} - Code: ${code} - Reason: ${reason}`)
-
-            let wsReference = this.wsLists.get(path)
-            if (wsReference) {
-
-                let reconnectSleep: number = this.reconnectSleep ?? 3000
-
-                console.log(`Reconnecting after ${reconnectSleep} ms`)
-                this.sleep(reconnectSleep).then(() => {
-                    this.connect(path, eventName)
-                })
+        try {
+            let URL = this.wsBaseURL
+            if (this.isTestNet) {
+                console.log("## Websocket: Test Net ##")
+                URL = this.wsBaseURLTest
             }
-        })
 
-        ws.on("error", (error) => console.log("Error Happens", error))
+            const ws = new WsClient(URL + path)
+
+            ws.on("open", () => {
+                try {
+                    console.log(`Connection Opened for: ${path}`)
+
+                    // Add to connection list
+                    this.wsLists.set(path, { ws, eventName })
+
+                    this.emit(eventName, ws, {
+                        api_key: this.api_key,
+                        api_secret: this.api_secret,
+                    })
+                } catch { }
+            })
+
+            ws.on("close", (code: number, reason: string) => {
+                try {
+                    console.log(`Connection closed for: ${path} - Code: ${code} - Reason: ${reason}`)
+
+                    let wsReference = this.wsLists.get(path)
+                    if (wsReference) {
+
+                        let reconnectSleep: number = this.reconnectSleep ?? 3000
+
+                        console.log(`Reconnecting after ${reconnectSleep} ms`)
+                        this.sleep(reconnectSleep).then(() => {
+                            this.connect(path, eventName)
+                        })
+                    }
+                } catch { }
+
+            })
+
+            ws.on("error", (error) => {
+                try {
+                    console.log("Error Happens", error)
+                } catch { }
+            })
+        } catch (error) {
+            console.log("Error connect:", error)
+        }
     }
 
     async userStream(listenKey: string, eventName: string) {
-        let path = "/ws/" + listenKey
-        this.connect(path, eventName)
+        try {
+            const path = "/ws/" + listenKey
+            this.connect(path, eventName)
+        } catch (error) {
+            console.log("Error userStream:", error)
+        }
     }
 }
